16a75e120781b2c7655a4c9a784d93e55f855f84
Raymond Zhou <somecrazyidiot@gmail.com>
2015-05-17 20:52:05 -0400

    Add Ship containsPoint() method.

--- src/js/ship.js
+++ src/js/ship.js
@@ -3,18 +3,30 @@ import THREE from 'three';
 const rotationMatrix = new THREE.Matrix4()
   .makeRotationFromEuler( new THREE.Euler( -Math.PI / 2, 0, 0 ) );
 
+const geometry = new THREE.CylinderGeometry( 0, 0.1, 0.5, 3 );
+geometry.applyMatrix( rotationMatrix );
+geometry.computeFaceNormals();
+geometry.computeVertexNormals();
+
+const material = new THREE.MeshPhongMaterial({
+  shading: THREE.FlatShading
+});
+
+const sphere = new THREE.Sphere();
+
 export default class Ship extends THREE.Mesh {
   constructor() {
-    const geometry = new THREE.CylinderGeometry( 0, 0.1, 0.5, 3 );
-    geometry.applyMatrix( rotationMatrix );
-    geometry.computeFaceNormals();
-    geometry.computeVertexNormals();
-
-    const material = new THREE.MeshPhongMaterial({
-      shading: THREE.FlatShading
-    });
-
-    super( geometry, material );
+    super( geometry, material.clone() );
     this.type = 'ship';
+  }
+
+  containsPoint( point ) {
+    if ( !this.geometry.boundingSphere ) {
+      this.geometry.computeBoundingSphere();
+    }
+
+    return sphere.copy( this.geometry.boundingSphere )
+      .applyMatrix( this.matrixWorld )
+      .containsPoint( point );
   }
 }
