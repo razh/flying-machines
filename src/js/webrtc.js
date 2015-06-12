51395dcb6be63df98424113188108416f7dd9242
Raymond Zhou <somecrazyidiot@gmail.com>
2015-06-12 02:59:31 -0400

    Add basic WebRTC messaging.

---
+++ src/js/webrtc.js
@@ -0,0 +1,65 @@
+import Peer from 'simple-peer';
+
+import { createState } from './client';
+import {
+  encodeClientState,
+  encodeServerState,
+  decodeClientMessage,
+  decodeServerMessage
+} from './encode';
+
+const INTERVAL = 16;
+
+function createPeerClient( peer, client, server ) {
+  const setState = createState( server );
+
+  const interval = setInterval(() => {
+    peer.send( encodeClientState( client ) );
+  }, INTERVAL );
+
+  peer.on( 'data', message => {
+    const state = decodeServerMessage( message );
+    if ( state ) {
+      setState( state );
+    }
+  });
+
+  peer.on( 'close', () => clearInterval( interval ) );
+}
+
+function createPeerServer( peer ) {
+  const state = [];
+  const id = state.length;
+
+  const interval = setInterval(() => {
+    try {
+      peer.send( encodeServerState( state ) );
+    } catch ( error ) {
+      console.error( error );
+      clearInterval( interval );
+    }
+  }, INTERVAL );
+
+  peer.on( 'data', message => {
+    const clientState = decodeClientMessage( message );
+    if ( clientState ) {
+      state[ id ] = clientState;
+    }
+  });
+
+  peer.on( 'close', () => {
+    clearInterval( interval );
+    delete state[ id ];
+  });
+}
+
+export function createPeer( client, server, options = {} ) {
+  const peer = new Peer( options );
+
+  peer.on( 'connect', () => {
+    createPeerClient( peer, client, server );
+    if ( options.initializer ) {
+      createPeerServer( peer );
+    }
+  });
+}
