33b3e69cc7790cf40f7c1f3168ce236d810bad7c
Raymond Zhou <somecrazyidiot@gmail.com>
2015-07-26 17:17:34 -0400

    Add Bullet sprite mesh.

--- src/js/bullet.js
+++ src/js/bullet.js
@@ -2,10 +2,39 @@ import THREE from 'three';
 import Entity from './entity';
 import { collisionMixin } from './collision';
 
-const geometry = new THREE.PlaneBufferGeometry( 1, 1 );
-geometry.applyMatrix( new THREE.Matrix4().makeRotationZ( Math.PI / 4 ) );
+const radius = 64;
 
-const material = new THREE.MeshBasicMaterial();
+const canvas = document.createElement( 'canvas' );
+const ctx = canvas.getContext( '2d' );
+
+canvas.width = 2 * radius;
+canvas.height = 2 * radius;
+
+const gradient = ctx.createRadialGradient(
+  radius, radius, 0,
+  radius, radius, radius
+);
+
+gradient.addColorStop( 0, '#fff' );
+gradient.addColorStop( 0.5, '#fff' );
+gradient.addColorStop( 0.75, '#f43' );
+gradient.addColorStop( 1, 'transparent' );
+
+ctx.fillStyle = gradient;
+ctx.fillRect( 0, 0, canvas.width, canvas.height );
+
+const geometry = new THREE.PlaneBufferGeometry( 1, 1 );
+
+const texture = new THREE.Texture( canvas );
+texture.needsUpdate = true;
+
+const material = new THREE.MeshBasicMaterial({
+  blending: THREE.AdditiveBlending,
+  depthWrite: false,
+  map: texture,
+  side: THREE.DoubleSide,
+  transparent: true
+});
 
 export default class Bullet extends Entity {
   constructor() {
@@ -13,7 +42,6 @@ export default class Bullet extends Entity {
     collisionMixin( this );
 
     this.type = 'bullet';
-    this.velocity = new THREE.Vector3();
     this.scale.setLength( 1 / 16 );
   }
 }
