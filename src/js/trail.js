6bf5976f5f4fe25743c2e17f1e2d2d5058127048
Raymond Zhou <somecrazyidiot@gmail.com>
2015-07-30 11:13:28 -0400

    Minimize Trail allocations of THREE.Vector3.

--- src/js/trail.js
+++ src/js/trail.js
@@ -1,17 +1,17 @@
 import THREE from 'three';
+import times from 'lodash/utility/times';
 
 const material = new THREE.LineBasicMaterial({
   blending: THREE.AdditiveBlending,
   color: '#f43',
-  depthTest: false,
-  linewidth: 10,
-  opacity: 0.5,
+  linewidth: 16,
+  opacity: 0.25,
   side: THREE.DoubleSide,
   transparent: true
 });
 
 export default class Trail extends THREE.Line {
-  constructor( count = 32 ) {
+  constructor( count = 64 ) {
     const geometry = new THREE.BufferGeometry();
     const vertices = new Float32Array( count * 3 );
     const attribute = new THREE.BufferAttribute( vertices, 3 );
@@ -19,21 +19,35 @@ export default class Trail extends THREE.Line {
 
     super( geometry, material );
 
-    this.count = count;
-    this.positions = [];
+    this.positions = times( count, () => new THREE.Vector3() );
+    this.offset = 0;
+
     this.frustumCulled = false;
   }
 
   track( target ) {
-    this.positions.unshift( target.position.clone() );
+    let index = 0;
 
-    while ( this.positions.length > this.count ) {
-      this.positions.pop();
+    for ( let i = this.offset; i < this.positions.length; i++ ) {
+      this.positions[i].toArray(
+        this.geometry.attributes.position.array,
+        3 * index
+      );
+
+      index++;
     }
 
-    this.positions.forEach( ( position, index ) => {
-      position.toArray( this.geometry.attributes.position.array, 3 * index );
-    });
+    for ( let i = 0; i < this.offset; i++ ) {
+      this.positions[i].toArray(
+        this.geometry.attributes.position.array,
+        3 * index
+      );
+
+      index++;
+    }
+
+    this.positions[ this.offset ].copy( target.position );
+    this.offset = ( this.offset + 1 ) % this.positions.length;
 
     this.geometry.attributes.position.needsUpdate = true;
   }
