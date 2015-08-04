33a12421f328a714ca51867bccd13f0c6b367df2
Raymond Zhou <somecrazyidiot@gmail.com>
2015-08-04 02:22:44 -0400

    Add targeting Reticle.

---
+++ src/js/reticle.js
@@ -0,0 +1,82 @@
+import THREE from 'three';
+import config from './config';
+
+const size = 128;
+
+// Segment length.
+const length = 32;
+
+const lineWidth = 8;
+const halfLineWidth = lineWidth / 2;
+
+const canvas = document.createElement( 'canvas' );
+const ctx = canvas.getContext( '2d' );
+
+canvas.width = size;
+canvas.height = size;
+
+/*
+  Counter-clockwise from top-left:
+
+    +--   --+
+    |       |
+
+    |       |
+    +--   --+
+ */
+
+ctx.beginPath();
+
+// Top left.
+ctx.moveTo( halfLineWidth + length, halfLineWidth );
+ctx.lineTo( halfLineWidth,          halfLineWidth );
+ctx.lineTo( halfLineWidth,          halfLineWidth + length );
+
+// Bottom left.
+ctx.moveTo( halfLineWidth,          size - halfLineWidth - length );
+ctx.lineTo( halfLineWidth,          size - halfLineWidth );
+ctx.lineTo( halfLineWidth + length, size - halfLineWidth );
+
+// Bottom right.
+ctx.moveTo( size - halfLineWidth - length, size - halfLineWidth );
+ctx.lineTo( size - halfLineWidth,          size - halfLineWidth );
+ctx.lineTo( size - halfLineWidth,          size - halfLineWidth - length );
+
+// Top right.
+ctx.moveTo( size - halfLineWidth,          halfLineWidth + length );
+ctx.lineTo( size - halfLineWidth,          halfLineWidth );
+ctx.lineTo( size - halfLineWidth - length, halfLineWidth );
+
+ctx.lineWidth = lineWidth;
+ctx.strokeStyle = '#fff';
+ctx.stroke();
+
+const texture = new THREE.Texture( canvas );
+texture.needsUpdate = true;
+
+const material = new THREE.SpriteMaterial({
+  depthTest: false,
+  map: texture,
+  rotation: Math.PI / 4,
+  transparent: true
+});
+
+const vector = new THREE.Vector3();
+
+export default class Reticle extends THREE.Sprite {
+  constructor() {
+    super( material );
+
+    this.stiffness = 0.2;
+    this.scale.setLength( 1 / 2 );
+  }
+
+  track( ship ) {
+    vector.set( 0, 0, -config.bullet.speed )
+      .applyQuaternion( ship.quaternion )
+      .add( ship.velocity )
+      .add( ship.position );
+
+    this.position.lerp( vector, this.stiffness );
+  }
+}
