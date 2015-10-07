a0763b81ed2da407a7d3e9936e138171f3750ec8
Raymond Zhou <somecrazyidiot@gmail.com>
2015-10-06 23:17:27 -0400

    Add SpriteTrail class.

--- src/js/trail.js
+++ src/js/trail.js
@@ -1,7 +1,7 @@
 import THREE from 'three';
 import times from 'lodash/utility/times';
 
-const material = new THREE.LineBasicMaterial({
+const lineMaterial = new THREE.LineBasicMaterial({
   blending: THREE.AdditiveBlending,
   color: '#f43',
   linewidth: 16,
@@ -17,7 +17,7 @@ export default class Trail extends THREE.Line {
     const attribute = new THREE.BufferAttribute( vertices, 3 );
     geometry.addAttribute( 'position', attribute );
 
-    super( geometry, material );
+    super( geometry, lineMaterial );
 
     // Offset from target.
     this.offset = new THREE.Vector3();
@@ -50,5 +50,105 @@ export default class Trail extends THREE.Line {
     this.start = ( this.start + 1 ) % this.positions.length;
 
     position.needsUpdate = true;
+  }
+}
+
+
+const size = 256;
+
+const canvas = document.createElement( 'canvas' );
+const ctx = canvas.getContext( '2d' );
+
+canvas.width = size;
+canvas.height = size;
+
+ctx.fillStyle = '#a32';
+ctx.fillRect( 0, 0, size, size );
+
+const texture = new THREE.Texture( canvas );
+texture.needsUpdate = true;
+
+const spriteMaterial = new THREE.SpriteMaterial({
+  blending: THREE.AdditiveBlending,
+  map: texture,
+  rotation: Math.PI / 4,
+  transparent: true,
+  opacity: 0.9
+});
+
+export class TrailSprite extends THREE.Sprite {
+  constructor() {
+    super( spriteMaterial );
+  }
+}
+
+export class SpriteTrail extends THREE.Group {
+  constructor( options = {} ) {
+    super();
+
+    const {
+      count = 64,
+      time = 0.5,
+      startScale = 0.02,
+      endScale = 0,
+      vertexDistance = 0.1
+    } = options;
+
+    this.time = time;
+    this.startScale = startScale;
+    this.endScale = endScale;
+    this.vertexDistance = vertexDistance;
+
+    let i = count;
+    while ( i-- ) {
+      const sprite = new TrailSprite();
+      sprite.visible = false;
+      this.add( sprite );
+    }
+
+    // Circular index.
+    this.index = 0;
+
+    this.previousPosition = null;
+    this.distance = 0;
+    this.offset = new THREE.Vector3();
+  }
+
+  next() {
+    const sprite = this.children[ this.index ];
+    sprite.visible = true;
+    sprite.scale.set( 1, 1, 1 );
+    this.index = ( this.index + 1 ) % this.children.length;
+    return sprite;
+  }
+
+  track( target ) {
+    if ( !this.previousPosition ) {
+      this.previousPosition = target.position.clone();
+      return;
+    }
+
+    this.distance += target.position.distanceTo( this.previousPosition );
+
+    if ( this.distance >= this.vertexDistance ) {
+      const sprite = this.next();
+
+      sprite.scale.set( this.startScale, this.startScale, this.startScale );
+      sprite.position.copy( this.offset )
+        .applyQuaternion( target.quaternion )
+        .add( target.position );
+
+      this.distance -= this.vertexDistance;
+    }
+
+    this.previousPosition.copy( target.position );
+  }
+
+  update( dt ) {
+    const ds = ( this.endScale - this.startScale ) / this.time * dt;
+
+    this.children.map( sprite => {
+      sprite.scale.addScalar( ds ).clampScalar( 0 )
+    });
   }
 }
