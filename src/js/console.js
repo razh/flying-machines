56a6b2cfceb8e4f8134001e6b03aeaf6598535d5
Raymond Zhou <somecrazyidiot@gmail.com>
2015-12-08 21:31:26 -0500

    Wrap debug console in function.

--- src/js/console.js
+++ src/js/console.js
@@ -1,40 +1,42 @@
-/* eslint-disable no-console */
-const element = document.createElement( 'div' );
-element.className = 'console';
-document.body.appendChild( element );
+export default function createConsole() {
+  const element = document.createElement( 'div' );
+  element.className = 'console';
+  document.body.appendChild( element );
 
-function render( element, state ) {
-  const { messages } = state;
+  function render( element, state ) {
+    const { messages } = state;
 
-  while ( messages.length > state.limit ) {
-    messages.shift();
+    while ( messages.length > state.limit ) {
+      messages.shift();
+    }
+
+    element.innerHTML = (
+      `<ul style="display: ${ state.show ? 'block' : 'none' }">
+        ${ messages.map( message => `<li>${ message }</li>` ).join('') }
+      </ul>`
+    );
   }
 
-  element.innerHTML = (
-    `<ul style="display: ${ state.show ? 'block' : 'none' }">
-      ${ messages.map( message => `<li>${ message }</li>` ).join('') }
-    </ul>`
-  );
-}
+  const state = {
+    messages: [],
+    limit: 20,
+    show: false
+  };
 
-const state = {
-  messages: [],
-  limit: 20,
-  show: false
-};
+  /* eslint-disable no-console */
+  const log = console.log.bind( console );
 
-const log = console.log.bind( console );
-
-console.log = ( ...args ) => {
-  log( ...args );
-  state.messages.push( [ ...args ] );
-  render( element, state );
-};
-
-document.addEventListener( 'keydown', event => {
-  // Backtick.
-  if ( event.keyCode === 192 ) {
-    state.show = !state.show;
+  console.log = ( ...args ) => {
+    log( ...args );
+    state.messages.push( [ ...args ] );
     render( element, state );
-  }
-})
+  };
+
+  document.addEventListener( 'keydown', event => {
+    // Backtick.
+    if ( event.keyCode === 192 ) {
+      state.show = !state.show;
+      render( element, state );
+    }
+  });
+}
