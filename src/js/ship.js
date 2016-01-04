97828dfb18ca0c9072877afda7176afb2d73779e
Raymond Zhou <somecrazyidiot@gmail.com>
2016-01-03 23:28:57 -0500

    Add LatheGeometry test for Ship.

--- src/js/ship.js
+++ src/js/ship.js
@@ -5,6 +5,8 @@ import Engine from './engine';
 import { defineLazyGetters } from './lazy';
 import { collisionMixin, CollisionShapes, CollisionGroups } from './collision';
 
+const createLathePoint = ([ x, z ]) => new THREE.Vector3( x, 0, z );
+
 const geometries = defineLazyGetters( {}, {
   basic() {
     const geometry = new THREE.CylinderGeometry( 0, 0.1, 0.5, 3 );
@@ -18,6 +20,41 @@ const geometries = defineLazyGetters( {}, {
 
   sphere() {
     return new THREE.IcosahedronGeometry( 0.2, 1 );
+  },
+
+  diamonds() {
+    const points = [
+      [ 0, 0 ],
+      [ 0.05, 0.1 ],
+      [ 0, 0.4 ]
+    ].map( createLathePoint );
+
+    const geometry = new THREE.LatheGeometry( points, 3 )
+      .rotateZ( Math.PI / 6 );
+
+    const boosterPoints = [
+      [ 0, 0 ],
+      [ 0.025, 0.04 ],
+      [ 0.025, 0.08 ],
+      [ 0, 0.2 ]
+    ].map( createLathePoint );
+
+    const leftBoosterGeometry = new THREE.LatheGeometry( boosterPoints, 4 );
+    const rightBoosterGeometry = new THREE.LatheGeometry( boosterPoints, 4 );
+
+    leftBoosterGeometry.translate( -0.1, 0, 0 );
+    rightBoosterGeometry.translate( 0.1, 0, 0 );
+
+    geometry.merge( leftBoosterGeometry );
+    geometry.merge( rightBoosterGeometry );
+
+    geometry.rotateX( Math.PI );
+    geometry.translate( 0, 0, 0.15 );
+
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+
+    return geometry;
   }
 });
 
