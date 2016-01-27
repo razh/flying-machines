31c1446a6cbd69dd4a8b49e586ee4a70315aecfa
Raymond Zhou <somecrazyidiot@gmail.com>
2016-01-26 23:35:49 -0500

    Add OBAFGKM star colors.
    https://en.wikipedia.org/wiki/Stellar_classification
    http://www.vendian.org/mncharity/dir3/starcolor/

--- src/js/starfield.js
+++ src/js/starfield.js
@@ -1,6 +1,18 @@
 import THREE from 'three';
 import { randomPointOnSphere } from './math';
 
+// https://en.wikipedia.org/wiki/Stellar_classification
+// http://www.vendian.org/mncharity/dir3/starcolor/
+const colors = [
+  '#9bb0ff',
+  '#aabfff',
+  '#cad8ff',
+  '#fbf8ff',
+  '#fff4e8',
+  '#ffddb4',
+  '#ffbd6f'
+];
+
 const vector = new THREE.Vector3();
 
 export default class Starfield extends THREE.BufferGeometry {
