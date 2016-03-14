8186482c8cee8931f8a3a379f689481053e74621
Raymond Zhou <somecrazyidiot@gmail.com>
2016-03-14 00:53:32 -0400

    Add bounding box operator function.

--- src/js/math.js
+++ src/js/math.js
@@ -19,3 +19,27 @@ export function lerp( a, b, t ) {
 export function inverseLerp( a, b, x ) {
   return ( x - a ) / ( b - a );
 }
+
+/*
+  Returns a function that invokes the callback on a vector, passing in the
+  position of the vector relative to the geometry bounding box, where
+  [ 0, 0, 0 ] is the center and extrema are in [ -1, 1 ].
+
+  For example, [ -1, 0, 0 ] if the vector is on the left face of the bounding
+  box, [ 0, 1, 0 ] if on the top face, etc.
+
+  The callback is invoked with four arguments: (vector, xt, yt, zt).
+ */
+export function operator( geometry, callback ) {
+  const box = geometry.computeBoundingBox();
+  const { min, max } = box;
+
+  return vector => {
+    callback(
+      vector,
+      2 * inverseLerp( min.x, max.x, vector.x ) - 1,
+      2 * inverseLerp( min.y, max.y, vector.y ) - 1,
+      2 * inverseLerp( min.z, max.z, vector.z ) - 1
+    );
+  };
+}
