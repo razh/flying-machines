50790a8376ec4113bb072b2b7f8c6849c4c07c4c
razh <somecrazyidiot@gmail.com>
2017-05-01 23:22:56 -0400

    Use first-order solution for target prediction.
    http://wiki.unity3d.com/index.php/Calculating_Lead_For_Projectiles

--- src/js/reticle.js
+++ src/js/reticle.js
@@ -151,6 +151,58 @@ export class Prediction extends THREE.Sprite {
   }
 }
 
+// http://wiki.unity3d.com/index.php/Calculating_Lead_For_Projectiles
+const predict = (() => {
+  'use strict';
+
+  const dx = new THREE.Vector3();
+  const dv = new THREE.Vector3();
+
+  return ( source, target, muzzleVelocity = 0 ) => {
+    dx.subVectors( target.position, source.position );
+    dv.subVectors( target.velocity, source.velocity );
+
+    const a = dv.lengthSq() - (muzzleVelocity * muzzleVelocity );
+    const b = 2 * dv.dot( dx );
+    const c = dx.lengthSq();
+
+    if ( a === 0 ) {
+      if ( b === 0 ) {
+        return;
+      }
+
+      return -c / b;
+    }
+
+    const d = ( b * b ) - ( 4 * a * c );
+
+    if ( d < 0 ) {
+      return;
+    }
+
+    const p = -b / ( 2 * a );
+
+    if ( d === 0 ) {
+      return p;
+    }
+
+    const q = Math.sqrt( d ) / ( 2 * a );
+
+    const t0 = p - q;
+    const t1 = p + q;
+
+    if ( t0 >= 0 && t1 >= 0 ) {
+      return Math.min( t0, t1 );
+    } else if ( t0 >= 0 ) {
+      return t0;
+    } else if ( t1 >= 0 ) {
+      return t1;
+    }
+
+    return;
+  };
+})();
+
 export class TargetingComputer extends THREE.Group {
   constructor( source, target ) {
     super();
@@ -178,23 +230,10 @@ export class TargetingComputer extends THREE.Group {
       return;
     }
 
-    /*
-      source  o
-              |\
-              | \
-      delta   |  \
-              |   \
-              |    \
-      target  o-----o prediction
-               v * t
-     */
-
-    const delta = vector
-      .subVectors( target.position, source.position );
-
-    const distance = delta.length();
-    const speed = source.velocity.dot( delta.normalize() ) + this.speed;
-    const time = distance / speed;
+    const time = predict( source, target, this.speed );
+    if ( !time ) {
+      return;
+    }
 
     // Update reticle.
     vector.set( 0, 0, -this.speed * time )
